210a972c6772fd0c5f8b4c2bd8b9fbe109e896a0
Mary Duarte <marygoncalvesduarte92@gmail.com>
2025-11-05 20:08:26 -0300

    Projeto está rodando

--- bomservico_be/src/main/java/unoeste/fipp/bomservico/BomservicoApplication.java
+++ bomservico_be/src/main/java/unoeste/fipp/bomservico/BomservicoApplication.java
@@ -2,7 +2,7 @@ package unoeste.fipp.bomservico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@SpringBootApplication
 public class BomservicoApplication {
 
 	public static void main(String[] args) {
